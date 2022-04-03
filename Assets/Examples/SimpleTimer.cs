66d14a0518deb0f559568022175625027f64bcd3
Serhii Fedorenko <fedorenkosergiy@gmail.com>
2022-04-03 20:25:04 +0300

    + one more example

---
+++ Assets/Examples/SimpleTimer.cs
@@ -0,0 +1,30 @@
+using Time = Moqunity.Abstract.UnityEngine.Time;//comment this line to get rid of Moqunity
+using UnityEngine;
+using System;
+
+public class SimpleTimer : MonoBehaviour
+{
+    private float fireTime = 0.0f;
+    private bool isActive = false;
+    private Action fireCallback = null;
+
+    private Time Time { get; } = Moqunity.Context.Factory.Time;//comment this line to get rid of Moqunity
+
+    public void Set(float time, Action callback)
+    {
+        if (isActive) return;
+        fireTime = Time.realtimeSinceStartup + time;
+        fireCallback = callback;
+        isActive = true;
+    }
+
+    private void Update()
+    {
+        if (!isActive) return;
+        if (Time.realtimeSinceStartup >= fireTime)
+        {
+            fireCallback?.Invoke();
+            isActive = false;
+        }
+    }
+}
---
+++ Assets/Tests/SimpleTimerTests.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using UnityEngine.TestTools;
+using Moqunity;
+using UnityEngine;
+using Moqunity.Fake.UnityEngine;
+using NUnit.Framework;
+using Time = Moqunity.Abstract.UnityEngine.Time;
+
+public class SimpleTimerTests
+{
+    private GameObject gameObject;
+
+    private class MyTyme : FakeTime
+    {
+        public float overriddenRealtime;
+
+        public override float realtimeSinceStartup => overriddenRealtime;
+
+        public void SetRealtimeSinceStartup(float value) => overriddenRealtime = value;
+    }
+
+    private class MyFactory : TestingFactory
+    {
+        private readonly Time overriddenTime;
+
+        public MyFactory(Time time) => overriddenTime = time;
+
+        protected override Time CreateTime() => overriddenTime;
+    }
+
+    [UnitySetUp]
+    public IEnumerator Setup()
+    {
+        gameObject = new GameObject();
+        yield return null;
+    }
+
+    [UnityTest]
+    public IEnumerator CheckCallbackWasFiredInTime()
+    {
+        var time = new MyTyme();
+        var factory = new MyFactory(time);
+        factory.Init();
+        using var context = new TestingContext(factory);
+        {
+            bool wasFired = false;
+            var timer = gameObject.AddComponent<SimpleTimer>();
+            yield return null;
+            timer.Set(10.0f, () => wasFired = true);
+            yield return null;
+            time.SetRealtimeSinceStartup(10.0f);
+            yield return null;
+            Assert.IsTrue(wasFired);
+            Object.Destroy(timer);
+        }
+    }
+
+    [UnityTest]
+    public IEnumerator CheckCallbackWasNotFiredBeforeTime()
+    {
+        var time = new MyTyme();
+        var factory = new MyFactory(time);
+        factory.Init();
+        using var context = new TestingContext(factory);
+        {
+            bool wasFired = false;
+            var timer = gameObject.AddComponent<SimpleTimer>();
+            yield return null;
+            timer.Set(10.0f, () => wasFired = true);
+            yield return null;
+            time.SetRealtimeSinceStartup(9.99f);
+            yield return null;
+            Assert.IsFalse(wasFired);
+            Object.Destroy(timer);
+        }
+    }
+
+    [UnityTearDown]
+    public IEnumerator TearDown()
+    {
+        Object.Destroy(gameObject);
+        yield return null;
+    }
+}
